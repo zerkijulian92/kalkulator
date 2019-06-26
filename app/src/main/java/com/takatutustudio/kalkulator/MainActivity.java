e41248093ebbb4ece9484b39517ab6bfe84724ef
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-06-26 21:45:52 +0700

    Perbaikan Activity dan mulai menulis program di mainActivity.java

--- app/src/main/java/com/takatutustudio/kalkulator/MainActivity.java
+++ app/src/main/java/com/takatutustudio/kalkulator/MainActivity.java
@@ -2,8 +2,16 @@ package com.takatutustudio.kalkulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextLayar;
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    Button buttonTambah, buttonKurang, buttonBagi, buttonKali;
+    Button buttonClear;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
